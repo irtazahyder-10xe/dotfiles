2a9eb106bf45f45613731f4e5d746de5aaddad9a
irtaza-10x <irtaza.hyder@10xengineers.ai>
2026-01-25 10:59:36 +0500

    Add themes to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,26 +1,45 @@
-# if status is-interactive
-# end
-
-# Color is ayu mirage
-#
-#----------- Aliases -----------
-abbr --add partition --position anywhere "/mnt/Badar_ki_Bhans/"
-
-#----------- NVIM Settings -----------
+# ======================== ENV VARS ======================== 
 export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+export EDITOR="/opt/nvim-linux-x86_64/bin/nvim"
+export RISCV="/home/lpt-10xe-10/Desktop/10xAssignments/RISCV64/riscv64-unknown-elf-toolchain"
+export PATH="$PATH:$RISCV/bin"
 
-#----------- VI Key Binds -----------
-function modified_vi_keybinds
+# ======================== FISH CONFIGS ======================== 
+fish_config theme choose "Catppuccin Mocha"
+
+# ======================== ALIAS & KEYBINDS ======================== 
+if status is-interactive
+    bind -M insert ctrl-r history-pager
+	abbr work_dir "~/Desktop/10xAssignments"
+	alias clang-format clang-format-21
+	alias ... "cd ../.."
+	alias .... "cd ../../.."
+	alias logisim "java -jar ~/Downloads/logisim-evolution-4.0.0-all.jar"
+	alias vcs "docker start synopsys_vcs-synopsys-1; docker attach synopsys_vcs-synopsys-1"
+end
+
+# ======================== CUSTOM FUNCTIONS ======================== 
+
+# alias apps "slack >/dev/null 2>&1 &; google-chrome >/dev/null 2>&1 &;"
+function apps -d "Runs slack and chrome";
+	slack >/dev/null 2>&1 &
+	google-chrome >/dev/null 2>&1 &
+end
+
+function modified_fish_vi_key_bindings
 	fish_vi_key_bindings
 	bind -M insert ctrl-f accept-autosuggestion
 end
+set -g fish_key_bindings modified_fish_vi_key_bindings
 
-set -g fish_key_bindings modified_vi_keybinds
+function mcd
+	mkdir -p $argv
+	cd $argv[1]
+end
 
-#----------- Git -----------
-function git_auto_push
-	set un "SyedMIrtazaHyder"
-	set pass "$(cat /mnt/Badar_ki_Bhans/github/githubToken | head -n1)"
+function git_auto_push -d "Push to remote repo"
+	set un "irtazahyder-10xe"
+	set pass "$(tail -n1 ~/.config/.github_token)"
 	set repo $(git remote -v | head -n1 | egrep -o "github.com/[^ ]+*")
 	set branch $1
 
@@ -28,5 +47,3 @@ function git_auto_push
 
 	git push $origin $branch
 end
-
-abbr --add gp --function git_auto_push
